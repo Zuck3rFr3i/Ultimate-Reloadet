9d3e7d4f3d26a58e3901f4b53300ffd2699e0a5d
Zuck3rFr3i <realzuck3r@web.de>
2017-01-03 07:32:37 +0100

    Add files via upload

---
+++ RESOURCEN/ultimate/anzeigen/prompt_server.lua
@@ -0,0 +1,30 @@
+promptPath = ":vio_stored_files/prompt/prompt.vio"
+if fileExists(promptPath) then
+	local promptFile = fileOpen ( promptPath, false )
+	local filesize = fileGetSize ( promptFile )
+	promptMainText = fileRead( promptFile, filesize )
+	fileClose ( promptFile )
+end
+
+function prompt ( player, text, time )
+
+	triggerClientEvent ( player, "prompt", player, text, time )
+end
+
+function changePrompt ( player, cmd, ... )
+
+	if vioGetElementData ( player, "adminlvl" ) >= 3 then
+		local msg = table.concat ( arg, " " )
+		
+		local file = fileCreate ( promptPath )
+		fileWrite ( file, msg )
+		fileClose ( file )
+		
+		promptMainText = msg
+		
+		dbExec ( handler, "UPDATE ?? SET ?? = ?", "userdata", "pred", "0" )
+		
+		outputChatBox ( "Erfolgreich geändert!", player, 0, 200, 0 )
+	end
+end
+addCommandHandler ( "changeprompt", changePrompt )
